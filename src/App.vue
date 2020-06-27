751ac43acaab9b8ccdbac6446aee901a24995c80
Ollie Boyd <ollyboyd@hotmail.co.uk>
2020-06-27 15:02:40 +0100

    everything working vefore refactor

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <l-map ref="myMap" style="height:100vh" :zoom="zoom" :center= "center"> 
+       <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+       <l-marker v-for="(station, index) in this.stationsLive":lat-lng="[station.lat, station.lon]" :key = "index" :icon="defaultIcon"></l-marker>
+    </l-map>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  
+  }, 
+  data(){
+    return {
+      stationsLive: [],
+      url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 13,
+      center: [55.950191, -3.187550],
+      defaultIcon: L.icon({
+        iconUrl: 'http://leafletjs.com/examples/custom-icons/leaf-green.png',
+        shadowUrl: 'http://leafletjs.com/examples/custom-icons/leaf-shadow.png',
+        iconSize:     [38, 95],
+        shadowSize:   [50, 64],
+        iconAnchor:   [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor:  [-3, -76]
+      })
+    
+    }
+  },
+  methods: {
+    fetchStationAvailability: function(){
+      const urlPaths = ['station_status.json', 'station_information.json'];
+      const requests = urlPaths.map(urlPath => fetch(`https://gbfs.urbansharing.com/edinburghcyclehire.com/${urlPath}`));
+    Promise.all(requests)
+    .then(responses => {
+      responses.forEach(response => console.log(response.url, response.status))
+      return responses;
+     })
+    .then(responses => Promise.all(responses.map(response => response.json())))
+    .then(responses => this.mergeObjectsByStationID(responses[0].data.stations, responses[1].data.stations))
+    .then(mergedResponses => this.stationsLive = mergedResponses)
+    .then(setTimeout(this.fetchStationAvailability, 60000))
+    },
+
+    mergeObjectsByStationID: function(locationsArr, usageArr){
+      const combinedArr = locationsArr.map((location) => {
+        const matchingUsageObject = usageArr.find(stationUsage => Object.is(stationUsage.station_id, location.station_id))
+        const merged = Object.assign(matchingUsageObject, location)
+        return merged
+      })
+      return combinedArr
+    }
+  },
+  mounted() {
+      this.fetchStationAvailability();
+      this.$nextTick(() => {
+        this.$refs.myMap.mapObject;
+      });
   }
 }
 </script>
 
 <style>
+body{
+  margin: 0px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0px;
+}
+#l-map {
+  height: 1000px;
+  width: 1000px;
 }
 </style>
