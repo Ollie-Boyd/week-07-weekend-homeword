a28525de775f3014d600d551afadf98bb60ce6b6
Ollie Boyd <ollyboyd@hotmail.co.uk>
2020-06-28 13:54:31 +0100

    finished move to leafletmarker.vue

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -1,14 +1,14 @@
 <template>
   <l-map ref="myMap" style="height:100vh" :zoom="zoom" :center= "center"> 
-       <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-       <l-marker v-for="(station, index) in this.stationsLive" :lat-lng="[station.lat, station.lon]" :key ="index">
-          <l-icon  :icon-size="[100,100]" :icon-url="customMarker"></l-icon>
-        </l-marker>       
+    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+    <leaflet-marker v-for="(station, index) in this.stationsLive" :station="station" :key ="index"></leaflet-marker>
+
   </l-map>
 </template>
 
 <script>
-import customMarker from '../assets/icons/station.svg'
+// import customMarker from '../assets/icons/station.svg'
+import LeafletMarker from "./LeafletMarker.vue"
 
 export default {
     name: "leaflet-map",
@@ -17,21 +17,24 @@ export default {
       
     },
     data(){
-    return {
-      customMarker,
-      url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
-      attribution:
-        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      zoom: 13,
-      center: [55.950191, -3.187550],
-      staticAnchor: [16, 37],
+      return {
+        // customMarker,
+        url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+        attribution:
+          '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        zoom: 13,
+        center: [55.950191, -3.187550],
+        staticAnchor: [16, 37],
      
       
+      }
+    },
+    components: {
+     "leaflet-marker": LeafletMarker
+    },
+    mounted(){
+      console.log(this.$refs.myMap.mapObject.getZoom())
     }
-  },
-  mounted(){
-    console.log(this.$refs.myMap.mapObject.getZoom())
-  }
 }
 </script>
 
---
+++ src/components/LeafletMarker.vue
@@ -0,0 +1,27 @@
+<template>
+    <l-marker :lat-lng="[station.lat, station.lon]">
+          <l-icon  :icon-size="[100,100]" :icon-url="customMarker"></l-icon>
+    </l-marker>    
+        
+
+</template>
+
+<script>
+
+import customMarker from '../assets/icons/station.svg'  //this is to byepass leaflet-2-vue bug 
+
+export default {
+    name: "leaflet-marker", 
+    props: ['station'],
+    data(){
+      return {
+        customMarker, //this is to byepass leaflet-2-vue bug 
+        staticAnchor: [16, 37],
+        }
+    },    
+}
+</script>
+
+<style>
+
+</style>
